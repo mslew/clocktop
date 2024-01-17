00f1a79b426bf3dfbdf024eede0ae8f14c589a24
mslew <mlew3223@gmail.com>
2024-01-17 11:13:13 -0600

    using an external package for better location grabbing

--- src/components/CurrWeather.tsx
+++ src/components/CurrWeather.tsx
@@ -1,48 +1,28 @@
 import { useState, useEffect } from "react";
+import { useGeolocated } from 'react-geolocated';
 
 function CurrWeather() {
+  interface LatLong {
+    lat: number;
+    long: number;
+  }
+
   const [temp, setTemp] = useState<number>(0);
   const [icon, setIcon] = useState<string>("");
-  const [lat, setLat] = useState<number>(0);
-  const [long, setLong] = useState<number>(0);
+  const [latLong, setLatLong] = useState<LatLong>({ lat: 0, long: 0 });
+  const {coords} = useGeolocated({
+    positionOptions:{
+      enableHighAccuracy: true,
+    },
+    userDecisionTimeout: 5000,
+  })
 
   useEffect(() => {
-    console.log("ran")
-    function fetchLocation() {
-      if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(
-          function (position) {
-            setLat(position.coords.latitude);
-            setLong(position.coords.longitude);
-            console.log(lat, long)
-            if (lat != 0) {
-              fetchWeather();
-            }
-          },
-          function (error) {
-            switch (error.code) {
-              case error.PERMISSION_DENIED:
-                console.error("User denied the request for geolocation.");
-                break;
-              case error.POSITION_UNAVAILABLE:
-                console.error("Location information is unavailable.");
-                break;
-              case error.TIMEOUT:
-                console.error("The request to get user location timed out.");
-                break;
-            }
-          }
-        );
-      } else {
-        console.error("Geolocation is not available in this browser.");
-      }
-    }
-
     async function fetchWeather() {
       const response = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=${
           import.meta.env.VITE_API_KEY_OG
-        }&q=${lat},${long}`,
+        }&q=${coords?.latitude},${coords?.longitude}`,
         {
           method: "POST",
           headers: {
@@ -57,17 +37,19 @@ function CurrWeather() {
         console.log(data);
         console.log(data.current.temp_f);
         setTemp(Math.round(data.current.temp_f));
-        let icon: string = data.current.condition.icon
-        let iconList: string[] = icon.split("/")
-        iconList[4] = "128x128"
-        icon = iconList.join("/")
+        let icon: string = data.current.condition.icon;
+        let iconList: string[] = icon.split("/");
+        iconList[4] = "128x128";
+        icon = iconList.join("/");
         setIcon(icon);
       } else {
         console.log("error fetching weather");
       }
     }
-    fetchLocation();
-  }, []);
+    if(coords != null){
+      fetchWeather();
+    }
+  }, [coords]);
 
   return (
     <div className="flex flex-col row-span-2 items-center justify-center gap-8">
